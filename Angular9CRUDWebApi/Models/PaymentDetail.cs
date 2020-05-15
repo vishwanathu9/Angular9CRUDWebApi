0ed80d091eda4cf614f0618cee1e5700b922582c
vishwanath upadhyay <vishwanathu9@gmail.com>
2020-05-15 08:32:18 +0530

    Add project files.

---
+++ Angular9CRUDWebApi/Models/PaymentDetail.cs
@@ -0,0 +1,28 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Angular9CRUDWebApi.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PMId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV { get; set; }
+    }
+}
